5f5b4bbe539ea58640e20a8022bbf674f2e696aa
kawsar-ahmmed9990 <kawsarahmmed9990@gmail.com>
2025-08-27 16:20:01 +0600

    add call button functionalities

--- index.js
+++ index.js
@@ -22,9 +22,26 @@ getElement("card-container").addEventListener("click", function (e) {
     if (currentCoin < 20) {
       alert("You don't have enough coins to make a call!");
     } else {
+      const currentTime = new Date().toLocaleTimeString();
       const presentCoin = currentCoin - 20;
       getElement("coin-number").innerText = presentCoin;
       alert(serviceName + " " + nmbr);
+      const historySectionContainer = getElement("history-section-container");
+      const newCart = document.createElement("div");
+
+      newCart.innerHTML = `<div
+              class="flex justify-between items-center bg-[#fafafa] p-2 mt-4 rounded-lg mb-2"
+            >
+              <div>
+                <h1 class="text-[20px] font-semibold">${serviceName}</h1>
+                <h2 class="text-[#5c5c5c] font-semibold">${nmbr}</h2>
+              </div>
+              <h1 class="text-[18px]">${currentTime}</h1>
+            </div>`;
+      historySectionContainer.append(newCart);
     }
   }
+});
+document.getElementById("clear-btn").addEventListener("click", function (e) {
+  getElement("history-section-container").innerText = "";
 });
