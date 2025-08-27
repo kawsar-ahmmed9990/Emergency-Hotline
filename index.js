b315dcc0a635f605a6103cc60de59ae117e9bc15
kawsar-ahmmed9990 <kawsarahmmed9990@gmail.com>
2025-08-27 17:11:18 +0600

    add copy button functionalities

--- index.js
+++ index.js
@@ -14,29 +14,30 @@ getElement("card-container").addEventListener("click", function (e) {
 getElement("card-container").addEventListener("click", function (e) {
   if (e.target.className.includes("call-btn")) {
     const callBtn = e.target;
-    const serviceName = callBtn.parentNode.parentNode.children[1].innerText;
+    const serviceName = callBtn.parentNode.parentNode.children[2].innerText;
+    const cardTitle = callBtn.parentNode.parentNode.children[1].innerText;
     const nmbr = callBtn.parentNode.parentNode.children[3].innerText;
 
     const currentCoin = getElement("coin-number").innerText;
 
     if (currentCoin < 20) {
-      alert("You don't have enough coins to make a call!");
+      alert("Insufficient coins! Minimum 20 coins required to make a call.");
     } else {
       const currentTime = new Date().toLocaleTimeString();
       const presentCoin = currentCoin - 20;
       getElement("coin-number").innerText = presentCoin;
-      alert(serviceName + " " + nmbr);
+      alert("Calling " + serviceName + " " + nmbr);
       const historySectionContainer = getElement("history-section-container");
       const newCart = document.createElement("div");
 
       newCart.innerHTML = `<div
-              class="flex justify-between items-center bg-[#fafafa] p-2 mt-4 rounded-lg mb-2"
+              class="flex justify-between gap-4 items-center bg-[#fafafa] p-2 mt-4 rounded-lg mb-2"
             >
               <div>
-                <h1 class="text-[20px] font-semibold">${serviceName}</h1>
+                <h1 class="text-[16px] font-semibold">${cardTitle}</h1>
                 <h2 class="text-[#5c5c5c] font-semibold">${nmbr}</h2>
               </div>
-              <h1 class="text-[18px]">${currentTime}</h1>
+              <h1 class="text-[16px]">${currentTime}</h1>
             </div>`;
       historySectionContainer.append(newCart);
     }
@@ -44,4 +45,15 @@ getElement("card-container").addEventListener("click", function (e) {
 });
 document.getElementById("clear-btn").addEventListener("click", function (e) {
   getElement("history-section-container").innerText = "";
+});
+getElement("card-container").addEventListener("click", function (e) {
+  if (e.target.className.includes("copy-btn")) {
+    const copyBtn = e.target;
+    const number = copyBtn.parentNode.parentNode.children[3].innerText;
+    const currentCopy = getElement("copy-cnt").innerText;
+    const totalCopy = Number(currentCopy) + 1;
+    getElement("copy-cnt").innerText = totalCopy;
+    alert("Number has been copied: " + number);
+    navigator.clipboard.writeText(number);
+  }
 });
